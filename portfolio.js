fde2536ec6b0dc7453100ce496331f5e6690bf5a
Mark McDermott <mcdermottsolutions@gmail.com>
2017-04-17 15:19:45 -0500

    changing port to 8001

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8000);
-console.log('serving portfolio on  8000');
+connect().use(serveStatic(__dirname)).listen(8001);
+console.log('serving portfolio on  8001');
 
