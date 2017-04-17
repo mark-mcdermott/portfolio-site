a1da8ad564d49ad29ff8ab0cb51becb495f75c04
Mark McDermott <mcdermottsolutions@gmail.com>
2017-04-17 15:06:25 -0500

    serving index.html as static from node

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,5 @@
-var http = require('http');
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("portfolio on 8000\n");
-});
-server.listen(8000);
-console.log("listening on 8000");
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8000);
+console.log('serving portfolio on  8000');
+
